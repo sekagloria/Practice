380ca9c17ac4bf566d1cdcb22f0a80d2ba1d4bd9
unknown <sekagloria>
2020-05-18 13:41:51 +0300

    initial

---
+++ if statements.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 13 22:16:30 2020
+
+@author: gloria
+"""
+food = "mango"
+food!="mango"
+age_0 = 22 
+age_1 = 18 
+age_0 >= 21 and age_1 >= 21
+
+marks = int(input(">"))
+if (marks >= 70):
+    print("grade A")
+elif (marks >= 60):
+   print("grade B")
+elif (marks >= 50):
+   print("grade C")
+elif (marks >=40):
+    print("grade D")
+else:
+   print("grade E")
