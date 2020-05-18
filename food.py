6c50b64c4b72b7d5158bfc1f20a383bb26dd9e09
unknown <sekagloria>
2020-05-18 13:14:46 +0300

    initial commit

---
+++ food.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 14 23:31:53 2020
+
+@author: gloria
+"""
+
+while True:
+        food =str(input(">"))
+        if food == "done":
+            break
+        else:
+            print ( " i have taken" +food+"." )
+        
